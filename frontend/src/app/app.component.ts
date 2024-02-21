97764db6c524a600c47d8407040d6ee85cd25710
Deepak <youcansend2ds@gmail.com>
2024-02-21 21:02:00 +0530

    creating the component2 for UI

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'UserDashboardUI';
+}
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent2 {
+  title = 'UserDashboardUI';
+}
