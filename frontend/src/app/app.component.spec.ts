a48c7aad2f7e58efb9a6b2f650dfb7b4baa5a728
Deepak <youcansend2ds@gmail.com>
2024-02-25 19:05:30 +0530

    Create app component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -8,12 +8,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
   it(`should have the 'UserDashboardUI' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
